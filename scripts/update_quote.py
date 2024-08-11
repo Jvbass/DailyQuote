775dfefcee8d632f4cc505bc094d6b27f9603d8d
Juan V. Pino Contreras <99030407+Jvbass@users.noreply.github.com>
2024-08-11 19:20:09 -0400

    Update update_quote.py

--- scripts/update_quote.py
+++ scripts/update_quote.py
@@ -6,10 +6,10 @@ with open('scripts/quotes.json', 'r') as f:
 
 frase = random.choice(data['frases'])
 
-with open('../index.html', 'r') as f:
+with open('index.html', 'r') as f:
     contenido = f.read()
 
 nuevo_contenido = contenido.replace('<h1>Frase actual</h1>', f'<h1>{frase}</h1>')
 
-with open('../index.html', 'w') as f:
+with open('index.html', 'w') as f:
     f.write(nuevo_contenido)
