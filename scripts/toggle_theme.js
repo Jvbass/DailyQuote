d4886e402bc9fde15a9a1d039ea1fb18e2a9494b
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2024-08-11 18:58:37 -0400

    First commit

---
+++ scripts/toggle_theme.js
@@ -0,0 +1,16 @@
+const toggleButton = document.getElementById("theme-toggle");
+const themeIcon = document.getElementById("theme-icon");
+const body = document.body;
+
+toggleButton.addEventListener("click", () => {
+  body.classList.toggle("dark-theme");
+
+  // Cambiar el ícono según el tema actual
+  if (body.classList.contains("dark-theme")) {
+    themeIcon.src = "./icons/moon-face.svg"; // Cambia a ícono de luna
+    themeIcon.alt = "Icono de luna";
+  } else {
+    themeIcon.src = "./icons/sun-face.svg"; // Cambia a ícono de sol
+    themeIcon.alt = "Icono de sol";
+  }
+});
---
+++ scripts/update_quote.py
@@ -0,0 +1,15 @@
+import json
+import random
+
+with open('quotes.json', 'r') as f:
+    data = json.load(f)
+
+frase = random.choice(data['frases'])
+
+with open('../index.html', 'r') as f:
+    contenido = f.read()
+
+nuevo_contenido = contenido.replace('<h1>Frase actual</h1>', f'<h1>{frase}</h1>')
+
+with open('../index.html', 'w') as f:
+    f.write(nuevo_contenido)
