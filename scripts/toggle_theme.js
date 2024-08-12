760717fecd89e10e78351122f8d56703bd2efea9
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2024-08-11 21:25:19 -0400

    modificacion script js para cambio de tema claro/oscuro

--- scripts/toggle_theme.js
+++ scripts/toggle_theme.js
@@ -1,16 +1,17 @@
-const toggleButton = document.getElementById("theme-toggle");
-const themeIcon = document.getElementById("theme-icon");
-const body = document.body;
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("theme-toggle");
+  const themeIcon = document.getElementById("theme-icon");
+  const body = document.body;
 
-toggleButton.addEventListener("click", () => {
-  body.classList.toggle("dark-theme");
-
-  // Cambiar el ícono según el tema actual
-  if (body.classList.contains("dark-theme")) {
-    themeIcon.src = "./icons/moon-face.svg"; // Cambia a ícono de luna
-    themeIcon.alt = "Icono de luna";
-  } else {
-    themeIcon.src = "./icons/sun-face.svg"; // Cambia a ícono de sol
-    themeIcon.alt = "Icono de sol";
-  }
+  toggleButton.addEventListener("click", () => {
+    body.classList.toggle("dark-theme");
+    // Cambiar el ícono según el tema actual
+    if (body.classList.contains("dark-theme")) {
+      themeIcon.src = "./icons/moon-face.svg"; // Cambia a ícono de luna
+      themeIcon.alt = "Icono de luna";
+    } else {
+      themeIcon.src = "./icons/sun-face.svg"; // Cambia a ícono de sol
+      themeIcon.alt = "Icono de sol";
+    }
+  });
 });
